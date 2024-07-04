f042ea175906e116f56e094418aa32ba649cb079
Greg-Mac <greg.toth@protonmail.com>
2024-07-04 21:30:53 +0100

    big model

--- notebooks/02-ai-label-data-generation.py
+++ notebooks/02-ai-label-data-generation.py
@@ -37,7 +37,7 @@ RANDOM_SEED = 42
 # PRECISION = torch.float32
 PRECISION_NAME = 'bfloat16'
 DEVICE = "cuda"
-LABELLER_MODEL = "bigscience/mt0-small"
+LABELLER_MODEL = "Qwen/Qwen2-72B-Instruct-GPTQ-Int4"
 # "Qwen/Qwen2-0.5B-Instruct"
 # "stabilityai/stablelm-2-zephyr-1_6b"
 # "microsoft/phi-1_5"
