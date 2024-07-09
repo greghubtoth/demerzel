dbb309b8d563587e315e46530b1c668c32d89061
Greg-Mac <greg.toth@protonmail.com>
2024-07-09 21:27:19 +0100

    test cot.

--- notebooks/02-ai-label-data-generation.py
+++ notebooks/02-ai-label-data-generation.py
@@ -69,32 +69,32 @@ config = {
 }
 
 ### ADAPTED_EXPEL_ET_AL
-config = {
-    'llm_model_name': LABELLER_MODEL,  # LABELLER_MODEL, # GEMMA_PATH, # 70B param model
-    'precision_name': PRECISION_NAME,
-    'device': DEVICE,
-    # 'dataset': data,
-    'batch_size': 2,
-    'run_id': RUN_ID,
-    'max_new_tokens': 512,
-    "n_retries": 1,
-    # Adapted Zhao et al. To generate insights, if not provided then data will dictate.
-    "insights_step_size": 4,
-    # Li et al. Negative examples are saved and can be retrieved for prompts.
-    "negative_examples:": True,
-    "embedding_model_name": "sentence-transformers/all-mpnet-base-v2",
-    "vdb_search_type": "similarity",
-    "max_vdb_documents": 5_000,
-}
+# config = {
+#     'llm_model_name': LABELLER_MODEL,  # LABELLER_MODEL, # GEMMA_PATH, # 70B param model
+#     'precision_name': PRECISION_NAME,
+#     'device': DEVICE,
+#     # 'dataset': data,
+#     'batch_size': 2,
+#     'run_id': RUN_ID,
+#     'max_new_tokens': 512,
+#     "n_retries": 1,
+#     # Adapted Zhao et al. To generate insights, if not provided then data will dictate.
+#     "insights_step_size": 4,
+#     # Li et al. Negative examples are saved and can be retrieved for prompts.
+#     "negative_examples:": True,
+#     "embedding_model_name": "sentence-transformers/all-mpnet-base-v2",
+#     "vdb_search_type": "similarity",
+#     "max_vdb_documents": 5_000,
+# }
 
 data_generator = Controller(
-    labelling_method=ADAPTED_EXPEL_ET_AL,  # ADAPTED_EXPEL_ET_AL,  # BASELINE_LEE_ET_AL,  # Tóth et al., (Ablation)
+    labelling_method=BASELINE_LEE_ET_AL,  # ADAPTED_EXPEL_ET_AL,  # BASELINE_LEE_ET_AL,  # Tóth et al., (Ablation)
     labelling_config=config,
     data_loader=HumanEvaluatedDataLoader,
 )
 
 if TESTING is True:
-    indices = random.sample(range(0, 92859), 12)
+    indices = random.sample(range(0, 92859), 200)
     # print(indices)
     data_generator.data_to_label["train"] = data_generator.data_to_label[
         "train"
