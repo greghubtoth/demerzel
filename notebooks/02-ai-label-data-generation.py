8d88d1c8be3d1bffb49ee06fae8a0603f33b0288
gtoth <greg.toth@genesys.com>
2024-07-13 15:31:13 +0100

    precision

--- notebooks/02-ai-label-data-generation.py
+++ notebooks/02-ai-label-data-generation.py
@@ -65,29 +65,29 @@ config = {
     'precision_name': PRECISION_NAME,
     'device': DEVICE,
     # 'dataset': data,
-    'batch_size': 4,
+    'batch_size': 6,
     'run_id': RUN_ID,
     'max_new_tokens': 512,
 }
 
 ### ADAPTED_EXPEL_ET_AL
-config = {
-    'llm_model_name': LABELLER_MODEL,  # LABELLER_MODEL, # GEMMA_PATH, # 70B param model
-    'precision_name': PRECISION_NAME,
-    'device': DEVICE,
-    # 'dataset': data,
-    'batch_size': 2,
-    'run_id': RUN_ID,
-    'max_new_tokens': 512,
-    "n_retries": 1,
-    # Adapted Zhao et al. To generate insights, if not provided then data will dictate.
-    "insights_step_size": 4,
-    # Li et al. Negative examples are saved and can be retrieved for prompts.
-    "negative_examples:": True,
-    "embedding_model_name": "sentence-transformers/all-mpnet-base-v2",
-    "vdb_search_type": "similarity",
-    "max_vdb_documents": 5_000,
-}
+# config = {
+#     'llm_model_name': LABELLER_MODEL,  # LABELLER_MODEL, # GEMMA_PATH, # 70B param model
+#     'precision_name': PRECISION_NAME,
+#     'device': DEVICE,
+#     # 'dataset': data,
+#     'batch_size': 2,
+#     'run_id': RUN_ID,
+#     'max_new_tokens': 512,
+#     "n_retries": 1,
+#     # Adapted Zhao et al. To generate insights, if not provided then data will dictate.
+#     "insights_step_size": 4,
+#     # Li et al. Negative examples are saved and can be retrieved for prompts.
+#     "negative_examples:": True,
+#     "embedding_model_name": "sentence-transformers/all-mpnet-base-v2",
+#     "vdb_search_type": "similarity",
+#     "max_vdb_documents": 5_000,
+# }
 
 data_generator = Controller(
     labelling_method=BASELINE_LEE_ET_AL,  # ADAPTED_EXPEL_ET_AL,  # BASELINE_LEE_ET_AL,  # Tóth et al., (Ablation)
@@ -96,7 +96,8 @@ data_generator = Controller(
 )
 
 if TESTING is True:
-    indices = random.sample(range(0, 92859), 20)
+    # clean up <|end|> in Gemma and other CoT bits and or others, split them and replace.
+    indices = random.sample(range(0, 92859), 50)
     # print(indices)
     data_generator.data_to_label["train"] = data_generator.data_to_label[
         "train"
