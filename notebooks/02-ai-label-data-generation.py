b9ad24306fa3c4cc0d5ea57c5bc2a7491edacbdd
gtoth <greg.toth@genesys.com>
2024-07-13 14:08:02 +0100

    Batch size.

--- notebooks/02-ai-label-data-generation.py
+++ notebooks/02-ai-label-data-generation.py
@@ -64,7 +64,7 @@ config = {
     'precision_name': PRECISION_NAME,
     'device': DEVICE,
     # 'dataset': data,
-    'batch_size': 10,
+    'batch_size': 6,
     'run_id': RUN_ID,
     'max_new_tokens': 512,
 }
