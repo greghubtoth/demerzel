f1351ba9e74a3935a6e83c59835f8270ba517b03
Greg-Mac <greg.toth@protonmail.com>
2024-07-17 20:23:27 +0100

    batch size to 1

--- notebooks/01-sft-modelling-reddit-summarisation.py
+++ notebooks/01-sft-modelling-reddit-summarisation.py
@@ -34,11 +34,11 @@ from pathlib import Path
 
 import pandas as pd
 
-TRAIN_BATCH_SIZE = 2
+TRAIN_BATCH_SIZE = 1
 EVALUATION_BATCH_SIZE = 4
 LEARNING_RATE = 1e-3
-LORA_PARAM_R = 24
-LORA_PARAM_ALPHA = 48
+LORA_PARAM_R = 8
+LORA_PARAM_ALPHA = 16
 LORA_PARAM_TARGET_MODULES = {
     "bigscience/mt0-small": ["q", "v"],
     "microsoft/phi-1_5": ["q_proj", "v_proj"],
