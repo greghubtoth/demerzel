654b7efa45f7e712f6d6f50b82165cd1920b53b9
Greg-Mac <greg.toth@protonmail.com>
2024-07-17 20:51:35 +0100

    smaller batch size

--- notebooks/02-ai-label-data-generation.py
+++ notebooks/02-ai-label-data-generation.py
@@ -66,7 +66,7 @@ config = {
     'precision_name': PRECISION_NAME,
     'device': DEVICE,
     # 'dataset': data,
-    'batch_size': 8,
+    'batch_size': 4,
     'run_id': RUN_ID,
     'max_new_tokens': 800,
 }
