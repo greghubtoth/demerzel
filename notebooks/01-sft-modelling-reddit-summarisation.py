88b5d979ca15ab6a1a636a2588470517a3447273
gtoth <greg.toth@genesys.com>
2024-07-18 10:18:43 +0100

    test smaller.

--- notebooks/01-sft-modelling-reddit-summarisation.py
+++ notebooks/01-sft-modelling-reddit-summarisation.py
@@ -46,7 +46,7 @@ LORA_PARAM_TARGET_MODULES = {
 # PRECISION = torch.float32
 PRECISION_NAME = 'float16'
 DEVICE = "cuda"  # 0 if torch.cuda.is_available() else "cpu"
-CHOSEN_MODEL = "microsoft/Phi-3-mini-4k-instruct"  # "bigscience/mt0-small" #"google/flan-t5-large"
+CHOSEN_MODEL = "microsoft/phi-1_5"  # "bigscience/mt0-small" #"google/flan-t5-large"
 TESTING = True
 RUN_ID = uuid.uuid4().hex
 print(RUN_ID)
