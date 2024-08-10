14483a17327020dc7b0bcc1aee70a46f6bb39a4b
Greg-Mac <greg.toth@protonmail.com>
2024-08-10 12:54:44 +0100

    FastLanguage model changes for inference.

--- notebooks/02-ai-label-data-generation.py
+++ notebooks/02-ai-label-data-generation.py
@@ -77,7 +77,7 @@ config = {
     'precision_name': PRECISION_NAME,
     'device': DEVICE,
     # 'dataset': data,
-    'batch_size': 4,
+    'batch_size': 2,
     'run_id': RUN_ID,
     'max_new_tokens': 512,
     "n_retries": 1,
