dcc91b28f94408e9936fd41f32af2e90fdeced65
Greg-Mac <greg.toth@protonmail.com>
2024-08-17 17:02:49 +0100

    PPO fixed.

--- nyx/constants.py
+++ nyx/constants.py
@@ -33,7 +33,7 @@ PROMPTS_COL = "prompts"
 # Reward Modelling paths
 RM_TRAIN_DATA_PATH = "{COMMON_OUTPUT_PATHS}/data/labelled-train-data"
 RM_OUTPUT_DIR = "{COMMON_OUTPUT_PATHS}/models/reward-modelling/events"
-RL_OUTPUT_DIR = "{COMMON_OUTPUT_PATHS}/models/reinforcement-learning/events"
+RL_OUTPUT_DIR = "{COMMON_OUTPUT_PATHS}/models/reinforcement-learning/peft-checkpoint-local"
 
 RM_PEFT_ADAPTER_PATH = (
     "{COMMON_OUTPUT_PATHS}/models/reward-modelling/peft-checkpoint-local"
