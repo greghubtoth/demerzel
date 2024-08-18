4529e5208a43c8c1991c4fb7e400e946fd09aec2
Greg-Mac <greg.toth@protonmail.com>
2024-08-18 13:19:23 +0100

    let's go.

--- notebooks/03-0-rlaif-rlhf-from-rm.py
+++ notebooks/03-0-rlaif-rlhf-from-rm.py
@@ -37,7 +37,7 @@ from tqdm import tqdm
 # from unsloth import FastLanguageModel
 # from unsloth import is_bfloat16_supported
 
-TESTING = True
+TESTING = False
 
 PRECISION_NAME = 'float16'
 DEVICE = "cuda"
