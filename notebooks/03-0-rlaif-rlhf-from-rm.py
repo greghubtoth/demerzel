673e7b55db2269e881295b9ed1f6464e6f00c0ae
Greg-Mac <greg.toth@protonmail.com>
2024-08-19 01:06:57 +0100

    kwargs.

--- notebooks/03-0-rlaif-rlhf-from-rm.py
+++ notebooks/03-0-rlaif-rlhf-from-rm.py
@@ -464,7 +464,7 @@ generation_kwargs = {
     "temperature": 0.6,
     "do_sample": True,
     "min_length": -1,
-    "eos_token_id": -1,
+    "eos_token_id": tokenizer.eos_token_id,
 }  # "top_k": 0.0, "top_p": 1.0 # "do_sample": True
 
 reward_kwargs = {
