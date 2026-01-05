063c93e433980638a12921a8693d75b67ac5fc3f
gtoth <greg.toth@genesys.com>
2026-01-05 17:52:21 +0000

    test in g5

--- notebooks/02-ai-label-data-generation.py
+++ notebooks/02-ai-label-data-generation.py
@@ -4,6 +4,7 @@
 # # AI labelled data generation and Reward Modelling
 
 import os
+
 # In[1]:
 import sys
 
@@ -22,13 +23,11 @@ sys.path.append(os.path.abspath(parent_dir))
 # python3 -m pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2
 
 import json
-import random
 import uuid
 
 from nyx.constants import COMMON_OUTPUT_PATHS, METRICS_PATH
 from nyx.data_generation import Controller
-from nyx.data_generation.settings import (ADAPTED_EXPEL_ET_AL,
-                                          BASELINE_LEE_ET_AL)
+from nyx.data_generation.settings import ADAPTED_EXPEL_ET_AL
 from nyx.data_loaders import HumanEvaluatedDataLoader
 
 # In[2]:
@@ -36,9 +35,10 @@ from nyx.data_loaders import HumanEvaluatedDataLoader
 
 RANDOM_SEED = 42
 # PRECISION = torch.float32
-PRECISION_NAME = 'float16'  # f16 for qwen and phi models.
+PRECISION_NAME = "float16"  # f16 for qwen and phi models.
 DEVICE = "cuda"
-LABELLER_MODEL = "google/gemma-2-27b-it"  # "microsoft/Phi-3-medium-128k-instruct"
+LABELLER_MODEL = "Qwen/Qwen2-0.5B-Instruct"
+# "google/gemma-2-27b-it"  # "microsoft/Phi-3-medium-128k-instruct"
 # "Qwen/Qwen2-72B-Instruct-GPTQ-Int4"
 # "Qwen/Qwen2-57B-A14B-Instruct-GPTQ-Int4"
 # "Qwen/Qwen2-7B-Instruct"
@@ -56,45 +56,46 @@ LABELLER_MODEL = "google/gemma-2-27b-it"  # "microsoft/Phi-3-medium-128k-instruc
 
 RUN_ID = uuid.uuid4().hex  # "test-multi-gpu-set-up"  #
 TESTING = True
-print(f'Employing model: {LABELLER_MODEL} on device: {DEVICE}.')
-print(f'RUN_ID: {RUN_ID}')
+print(f"Employing model: {LABELLER_MODEL} on device: {DEVICE}.")
+print(f"RUN_ID: {RUN_ID}")
 # In[ ]:
 
 # BASELINE_LEE_ET_AL
+config = {
+    "llm_model_name": LABELLER_MODEL,  # LABELLER_MODEL, # GEMMA_PATH, # 70B param model
+    "precision_name": PRECISION_NAME,
+    "device": DEVICE,
+    # 'dataset': data,
+    "batch_size": 4,
+    "run_id": RUN_ID,
+    "max_new_tokens": 800,
+}
+
+### ADAPTED_EXPEL_ET_AL
 # config = {
 #     'llm_model_name': LABELLER_MODEL,  # LABELLER_MODEL, # GEMMA_PATH, # 70B param model
 #     'precision_name': PRECISION_NAME,
 #     'device': DEVICE,
 #     # 'dataset': data,
-#     'batch_size': 4,
+#     'batch_size': 2,
 #     'run_id': RUN_ID,
-#     'max_new_tokens': 800,
+#     'max_new_tokens': 512,
+#     "n_retries": 1,
+#     # Adapted Zhao et al. To generate insights, if not provided then data will dictate.
+#     "insights_step_size": 40,
+#     # Tóth et al. turning ExpeL from MC to n-step method.
+#     "insights_early_stopping": 500,
+#     # Li et al. Negative examples are saved and can be retrieved for prompts.
+#     "utilise_examples": True,
+#     "negative_examples": True,
+#     "embedding_model_name": "sentence-transformers/all-mpnet-base-v2",
+#     "vdb_search_type": "similarity",
+#     "max_vdb_documents": 5_0000,
 # }
-
-### ADAPTED_EXPEL_ET_AL
-config = {
-    'llm_model_name': LABELLER_MODEL,  # LABELLER_MODEL, # GEMMA_PATH, # 70B param model
-    'precision_name': PRECISION_NAME,
-    'device': DEVICE,
-    # 'dataset': data,
-    'batch_size': 2,
-    'run_id': RUN_ID,
-    'max_new_tokens': 512,
-    "n_retries": 1,
-    # Adapted Zhao et al. To generate insights, if not provided then data will dictate.
-    "insights_step_size": 40,
-    # Tóth et al. turning ExpeL from MC to n-step method.
-    "insights_early_stopping": 500,
-    # Li et al. Negative examples are saved and can be retrieved for prompts.
-    "utilise_examples": True,
-    "negative_examples": True,
-    "embedding_model_name": "sentence-transformers/all-mpnet-base-v2",
-    "vdb_search_type": "similarity",
-    "max_vdb_documents": 5_0000,
-}
 # print(config)
 data_generator = Controller(
-    labelling_method=ADAPTED_EXPEL_ET_AL,  # ADAPTED_EXPEL_ET_AL,  # BASELINE_LEE_ET_AL,  # Tóth et al., (Ablation)
+    labelling_method=f"{ADAPTED_EXPEL_ET_AL}_vllm",
+    # ADAPTED_EXPEL_ET_AL,  # BASELINE_LEE_ET_AL,  # Tóth et al., (Ablation)
     labelling_config=config,
     data_loader=HumanEvaluatedDataLoader,
 )
@@ -141,28 +142,28 @@ METRICS_PATH = METRICS_PATH.format(COMMON_OUTPUT_PATHS=COMMON_OUTPUT_PATHS)
 if not os.path.exists(METRICS_PATH):
     os.makedirs(METRICS_PATH)
 
-data_path = f'{METRICS_PATH}/data-generation-info.json'
+data_path = f"{METRICS_PATH}/data-generation-info.json"
 
-drop_info = ['dataset', 'run_id', 'llm_model_name', 'precision_name']
+drop_info = ["dataset", "run_id", "llm_model_name", "precision_name"]
 
 results_dict = {
-    'run_id': RUN_ID,
-    'labeller_model': LABELLER_MODEL,
-    'precision': PRECISION_NAME,
-    'duration': data_generator.labelling_duration,
-    'n_gpus_available': data_generator.n_gpus_available,
-    'gpu_type': data_generator.gpu_type,
-    'method': data_generator.labelling_method,
-    'run_configuration': {
+    "run_id": RUN_ID,
+    "labeller_model": LABELLER_MODEL,
+    "precision": PRECISION_NAME,
+    "duration": data_generator.labelling_duration,
+    "n_gpus_available": data_generator.n_gpus_available,
+    "gpu_type": data_generator.gpu_type,
+    "method": data_generator.labelling_method,
+    "run_configuration": {
         key: value
         for key, value in data_generator.labelling_config.items()
         if key not in drop_info
     },
 }
 
-with open(data_path, 'w') as file:
+with open(data_path, "w") as file:
     json.dump(results_dict, file)
 
-print('Data generation done and the configuration info is saved.')
+print("Data generation done and the configuration info is saved.")
 print(data_path)
 # # END
